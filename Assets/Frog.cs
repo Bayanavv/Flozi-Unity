e58466546e6e8955f86d3df2d9751ec30033e2ab
bayanavv <bayanav.sergen@icloud.com>
2020-03-25 10:35:28 +0000

    progress

--- Assets/Frog.cs
+++ Assets/Frog.cs
@@ -4,5 +4,91 @@ using UnityEngine;
 
 public class Frog : MonoBehaviour
 {
+    [SerializeField] private float leftCap;
+    [SerializeField] private float rightCap;
 
+    [SerializeField] private float jumpLength = 10f;
+    [SerializeField] private float jumpHight = 15f;
+    [SerializeField] private LayerMask ground;
+    private Collider2D coll;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private bool facingLeft = true;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Transition from Jump to fall
+        if(anim.GetBool("Jumping"))
+        {
+            if(rb.velocity.y < .1)
+            {
+                anim.SetBool("Falling", true);
+                anim.SetBool("Jumping", false);
+            }
+        }
+
+        //Transition from fall to idle
+        if(coll.IsTouchingLayers(ground) && anim.GetBool("Falling"))
+        {
+            anim.SetBool("Falling", false);
+        }
+    }
+
+    private void Move()
+    {
+        if (facingLeft)//if i facing left
+        {
+            if (transform.position.x > leftCap) // check if my position is beonde left cup
+            {
+                //Make sure sprite is facong right location, and if it is not, then face the right direction
+                if (transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                //Test to see if am on the ground, if so jump
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(-jumpLength, jumpHight);
+                    anim.SetBool("Jumping", true);//after testing that the frog touching the groin he jumping and animetion changes
+                }
+            }
+            else//face right
+            {
+                facingLeft = false;
+            }
+
+        }
+
+        else //facing right
+        {
+            if (transform.position.x < rightCap)
+            {
+                if (transform.localScale.x != -1)
+                {
+                    transform.localScale = new Vector3(-1, 1);
+                }
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(jumpLength, jumpHight);
+                    anim.SetBool("Jumping", true);
+                }
+            }
+            else
+            {
+                facingLeft = true;
+            }
+
+        }
+    }
 }
+
+
+
