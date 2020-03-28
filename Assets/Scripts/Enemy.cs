8bb5c84c8daa8c7e0e9859889011f7b05861c595
bayanavv <bayanav.sergen@icloud.com>
2020-03-28 13:52:36 +0300

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    protected Animator anim;
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void JumpOn()
     {
-        
+        anim.SetTrigger("Death");
+    }
+
+    private void Death()
+    {
+        Destroy(this.gameObject);
     }
 }
