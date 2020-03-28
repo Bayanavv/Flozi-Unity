c396b696d19ab79b849daaeb91b2f77a4b3db9a2
bayanavv <bayanav.sergen@icloud.com>
2020-03-28 17:31:23 +0300

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,12 @@ public class Enemy : MonoBehaviour
 {
 
     protected Animator anim;
+    protected Rigidbody2D rb;
     // Start is called before the first frame update
     protected virtual void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class Enemy : MonoBehaviour
     public void JumpOn()
     {
         anim.SetTrigger("Death");
+        rb.velocity = Vector2.zero;//same as (new vector(0,0))
     }
 
     private void Death()
